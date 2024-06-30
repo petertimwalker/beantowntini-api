126b23f57b976893c1ff48e643e2b869843b8b73
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-06-30 17:12:52 -0400

    format and Signed up successfully. from postman

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsControlleri
+class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionsFix
-  
+
   respond_to :json
 
   private
@@ -10,12 +10,12 @@ class Users::RegistrationsController < Devise::RegistrationsControlleri
   def respond_with(current_user, _opts = {})
     if resource.persisted?
       render json: {
-        status: {code: 200, message: 'Signed up successfully.'},
+        status: { code: 200, message: 'Signed up successfully.' },
         data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
       }
     else
       render json: {
-        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+        status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
     end
   end
